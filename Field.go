6716f1aaec73659ff1c5b7600d54ad3157bad3de
Piotr Pszczolkowski <piotr@beesoft.pl>
2017-06-23 11:05:12 +0200

    use reflection in value conversion

--- Field.go
+++ Field.go
@@ -26,9 +26,6 @@
 package BeeSQLite
 
 import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
 	"log"
 )
 
@@ -47,7 +44,7 @@ const (
 type Field struct {
 	Name      string
 	valueType ValueType
-	data      []byte
+	value     interface{}
 }
 
 // BindName - computed name used in binding
@@ -59,83 +56,55 @@ func (f *Field) BindName() string {
 func (f *Field) SetValue(v interface{}) {
 	// fmt.Println(reflect.ValueOf(v).Kind())
 
-	switch x := v.(type) {
+	f.value = v
+	switch v.(type) {
 	case string:
-		f.data = []byte(x)
 		f.valueType = Text
 	case int:
-		f.data = convert(int64(x))
 		f.valueType = Integer
 	case float32:
-		f.data = convert(float64(x))
 		f.valueType = Float
 	case float64:
-		f.data = convert(x)
 		f.valueType = Float
-	case bool:
-		bi := 1
-		if !x {
-			bi = 0
-		}
-		f.data = convert(int64(bi))
-		f.valueType = Integer
 	case []byte:
-		f.data = x
 		f.valueType = Blob
 	default:
-		f.data = []byte{}
 		f.valueType = Null
 	}
 }
 
-func convert(v interface{}) []byte {
-	buff := new(bytes.Buffer)
-	err := binary.Write(buff, binary.LittleEndian, v)
-	if err != nil {
-		fmt.Println("Field.convert: " + err.Error())
-		return []byte{}
-	}
-	return buff.Bytes()
-
-}
-
 // Int - return value as int
 func (f *Field) Int() int {
-	var value int64
-	buff := bytes.NewReader(f.data)
-	err := binary.Read(buff, binary.LittleEndian, &value)
-	if err != nil {
-		log.Fatal(err)
+	v, ok := f.value.(int)
+	if !ok {
+		log.Fatal()
 	}
-	return int(value)
+	return v
 }
 
 // Float - returns value as float64
 func (f *Field) Float() float64 {
-	var value float64
-	buff := bytes.NewReader(f.data)
-	err := binary.Read(buff, binary.LittleEndian, &value)
-	if err != nil {
-		log.Fatal(err)
+	v, ok := f.value.(float64)
+	if !ok {
+		log.Fatal()
 	}
-	return value
+	return v
 }
 
 // String - return value as string
 func (f *Field) String() string {
-	return string(f.data)
+	v, ok := f.value.(string)
+	if !ok {
+		log.Fatal()
+	}
+	return v
 }
 
 // Blob - return value as array of bytes
 func (f *Field) Blob() []byte {
-	return f.data
-}
-
-// Bool - return value as bool
-func (f *Field) Bool() bool {
-	v := f.Int()
-	if v == 1 {
-		return true
+	v, ok := f.value.([]byte)
+	if !ok {
+		log.Fatal()
 	}
-	return false
+	return v
 }
